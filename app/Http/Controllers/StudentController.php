5b324c64f43f920ac1d422300b9ff3fcaf6e502d
kolejuniti <kolejuniti.lms@uniti.edu.my>
2022-08-18 16:46:37 +0800

    update student controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -153,15 +153,15 @@ class StudentController extends Controller
     public function courseContent()
     {
 
-        $lecturer = DB::table('user_subjek')->join('student_subjek', 'user_subjek.id','student_subjek.group_id')
-                    ->join('subjek', 'user_subjek.course_id', 'subjek.sub_id')
+        $lecturer = DB::table('student_subjek')
+                    ->join('subjek', 'student_subjek.courseid', 'subjek.sub_id')
                     ->where([
                         ['student_subjek.student_ic', Session::get('StudInfo')->ic],
                         ['subjek.id', request()->id],
                         ['student_subjek.sessionid', Session::get('SessionID')],
-                    ])->select('user_subjek.*')->first();
+                    ])->select('student_subjek.*')->first();
         
-        dd(Session::get('SessionID'));
+        dd($lecturer);
 
         $subid = DB::table('subjek')->where('id', request()->id)->pluck('sub_id');
 
