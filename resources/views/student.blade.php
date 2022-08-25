437a7fdf0d8de5e98e86ae35f23db8e00dc878ba
kolejuniti <kolejuniti.lms@uniti.edu.my>
2022-08-25 15:15:52 +0800

    update student view

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -109,9 +109,9 @@
                             <p class="card-text text-gray-600 pt-5">
                               <strong>Code</strong> {{ ucwords($key->course_code) }}
                             </p>
-                            <p class="card-text text-gray-600">
+                            <!--<p class="card-text text-gray-600">
                               <strong>Program</strong> {{ ucwords($key->progname) }}
-                            </p>
+                            </p>-->
                           </div>
                         </div>
                     </div>
