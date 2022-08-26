f75738572fd27643428b4469e9724b3138337c8e
kolejuniti <kolejuniti.lms@uniti.edu.my>
2022-08-26 17:55:58 +0800

    update lct controller

--- app/Http/Controllers/LecturerController.php
+++ app/Http/Controllers/LecturerController.php
@@ -2149,7 +2149,7 @@ class LecturerController extends Controller
         }
 
 
-        dd($list);
+        dd($lists);
             
         return view('lecturer.class.attendancereport', compact('lists', 'students', 'group', 'date'));
 
