a9486415f7d672cc638a16ef452075c6ad906cb0
kolejuniti <kolejuniti.lms@uniti.edu.my>
2022-09-01 17:18:52 +0800

    update schedule

--- resources/views/layouts/lecturer.blade.php
+++ resources/views/layouts/lecturer.blade.php
@@ -237,7 +237,7 @@
 					<a href="{{ route('lecturer') }}" class="{{ (route('lecturer') == Request::url()) ? 'active' : ''}}"><i data-feather="bookmark"></i><span>Course</span></a>
 				</li>
         		<li>
-					<a href="{{ Storage::disk('local')->url('storage/classschedule/index.htm'); }}" class="{{ (route('lecturer') == Request::url()) ? 'active' : ''}}"><i data-feather="bookmark"></i><span>Schedule</span></a>
+					<a href="{{ Storage::disk('local')->url('classschedule/index.htm'); }}" class="{{ (route('lecturer') == Request::url()) ? 'active' : ''}}"><i data-feather="bookmark"></i><span>Schedule</span></a>
 				</li> 
 			  </ul>
 			  <div class="sidebar-widgets">
