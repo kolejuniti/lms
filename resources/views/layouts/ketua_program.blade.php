d15bda11bdcf3ba876f93d9e258ce66bd5171676
kolejuniti <kolejuniti.lms@uniti.edu.my>
2022-09-13 16:57:35 +0800

    update jadual

--- resources/views/layouts/ketua_program.blade.php
+++ resources/views/layouts/ketua_program.blade.php
@@ -285,7 +285,7 @@
 					<a href="{{ route('lecturer') }}" class="{{ (route('lecturer') == Request::url()) ? 'active' : ''}}"><i data-feather="bookmark"></i><span>Course</span></a>
 				</li>
         		<li>
-					<a href="{{ Storage::disk('linode')->url('classschedule/index.htm') }}" target="_blank" class="{{ (route('lecturer') == Request::url()) ? 'active' : ''}}"><i data-feather="bookmark"></i><span>Schedule</span></a>
+					<a href="{{ Storage::disk('linode')->url('classschedule/index.htm') }}" target="_blank" class="{{ (route('lecturer') == Request::url()) ? 'active' : ''}}"><i data-feather="layout"></i><span>Schedule</span></a>
 				</li>   
         		<!--<li>
 					<a href="{{ url('storage/classschedule/index.htm') }}" target="_blank" class="{{ (route('lecturer') == Request::url()) ? 'active' : ''}}"><i data-feather="layout"></i><span>Schedule</span></a>
