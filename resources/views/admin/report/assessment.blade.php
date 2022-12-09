193b94bcd30b9c1956c70fc7af16652ae7e7b4ad
kolejuniti <kolejuniti.lms@uniti.edu.my>
2022-12-09 14:57:51 +0800

    update admin report

---
+++ resources/views/admin/report/assessment.blade.php
@@ -0,0 +1,149 @@
+@extends('../layouts.finance')
+
+@section('main')
+<!-- Content Header (Page header) -->
+<div class="content-wrapper" style="min-height: 695.8px;">
+  <div class="container-full">
+  <!-- Content Header (Page header) -->	  
+  <div class="content-header">
+    <div class="d-flex align-items-center">
+      <div class="me-auto">
+        <h4 class="page-title">Report</h4>
+        <div class="d-inline-block align-items-center">
+          <nav>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+              <li class="breadcrumb-item" aria-current="page">Report</li>
+              <li class="breadcrumb-item active" aria-current="page">Assessment</li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Assessment Report</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card mb-3">
+                <div class="card-header">
+                  <b>Search Assessment</b>
+                </div>
+                <div class="card-body">
+                  <div class="row">
+                      <div class="col-md-3">
+                        <div class="form-group">
+                          <label class="form-label" for="name">From</label>
+                          <input type="date" class="form-control" id="from">
+                        </div>
+                      </div>
+                      <div class="col-md-3">
+                        <div class="form-group">
+                          <label class="form-label" for="name">To</label>
+                          <input type="date" class="form-control" id="to">
+                        </div>
+                    </div>
+                  </div>
+                  <div id="form-assessment">
+              
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+</div>
+
+<script src="{{ asset('assets/assets/vendor_components/ckeditor/ckeditor.js') }}"></script>
+<script src="https://cdn.ckeditor.com/ckeditor5/31.0.0/classic/ckeditor.js"></script>
+<script type="text/javascript">
+
+$(document).on('change', '#from', async function(e){
+    selected_from = $(e.target).val();
+
+    await getAssessment(selected_from,selected_to);
+});
+
+$(document).on('change', '#to', async function(e){
+    selected_to = $(e.target).val();
+
+    await getAssessment(selected_from,selected_to);
+});
+
+
+function getAssessment(from,to)
+{
+    return $.ajax({
+            headers: {'X-CSRF-TOKEN':  $('meta[name="csrf-token"]').attr('content')},
+            url      : "{{ url('admin/report/assessment/getAssessment') }}",
+            method   : 'POST',
+            data 	 : {from: from, to: to},
+            error:function(err){
+                alert("Error");
+                console.log(err);
+            },
+            success  : function(data){
+              var d = new Date();
+
+              var month = d.getMonth()+1;
+              var day = d.getDate();
+
+              var output = d.getFullYear() + '/' +
+                  (month<10 ? '0' : '') + month + '/' +
+                  (day<10 ? '0' : '') + day;
+
+
+                $('#form-assessment').html(data);
+            
+                $('#complex_header').DataTable({
+                  dom: 'lBfrtip', // if you remove this line you will see the show entries dropdown
+                  
+                  buttons: [
+                    {
+                        extend: 'excelHtml5',
+                        messageTop: output,
+                        title: 'Excel' + '-' + output,
+                        text:'Export to excel'
+                        //Columns to export
+                        //exportOptions: {
+                       //     columns: [0, 1, 2, 3,4,5,6]
+                       // }
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        title: 'PDF' + '-' + output,
+                        text: 'Export to PDF'
+                        //Columns to export
+                        //exportOptions: {
+                       //     columns: [0, 1, 2, 3, 4, 5, 6]
+                      //  }
+                    }
+                  ],
+                });
+                //$('#student').selectpicker('refresh');
+
+                "use strict";
+                ClassicEditor
+                .create( document.querySelector( '#commenttxt' ),{ height: '25em' } )
+                .then(newEditor =>{editor = newEditor;})
+                .catch( error => { console.log( error );});
+            }
+        });
+}
+
+
+</script>
+@endsection
