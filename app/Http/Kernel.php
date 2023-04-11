2da0672a89d41e7148aa9e9603204685b42f3da6
hafiy <hafiyyaimann1998@gmail.com>
2023-04-11 09:07:04 +0800

    upt kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CheckUserActivity::class,
         ],
 
         'api' => [
@@ -64,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'preventBackHistory' => \App\Http\Middleware\PreventBackHistory::class,
+        'checkUserActivity' => \App\Http\Middleware\CheckUserActivity::class,
     ];
 }
