d72a3f80ad6e83d87235a499f9237b1c69f32851
hafiy <hafiyyaimann1998@gmail.com>
2023-06-26 17:50:32 +0800

    upt coop

---
+++ resources/views/coop/voucher/dailyReport.blade.php
@@ -0,0 +1,207 @@
+@extends('../layouts.coop')
+
+@section('main')
+
+<!-- Content Header (Page header) -->
+<div class="content-wrapper" style="min-height: 695.8px;">
+  <div class="container-full">
+    <!-- Content Header (Page header) -->	  
+    <div class="content-header">
+      <div class="d-flex align-items-center">
+        <div class="me-auto">
+          <h4 class="page-title">Daily Report</h4>
+          <div class="d-inline-block align-items-center">
+            <nav>
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+                <li class="breadcrumb-item" aria-current="page">Dashboard</li>
+                <li class="breadcrumb-item active" aria-current="page">Daily Report</li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+      </div>
+      @if($errors->any())
+        <div class="form-group">
+            <div class="alert alert-success">
+              <span>{{$errors->first()}} </span>
+            </div>
+        </div>
+      @endif
+    </div>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- /.card-header -->
+      <div class="card card-primary">
+        <div class="card-header">
+          <b>Search Student</b>
+          <button id="printButton" class="waves-effect waves-light btn btn-primary btn-sm">
+            <i class="ti-printer"></i>&nbsp Print
+          </button>
+        </div>
+        <div class="card-body">
+          <div class="row">
+              <div class="col-md-6">
+                  <div class="form-group">
+                  <label class="form-label" for="from">FROM</label>
+                  <input type="date" class="form-control" id="from" name="from">
+                  </div>
+              </div>
+              <div class="col-md-6">
+                  <div class="form-group">
+                  <label class="form-label" for="name">TO</label>
+                  <input type="date" class="form-control" id="to" name="to">
+                  </div>
+              </div>
+          </div>
+          <div id="form-student">
+            
+  
+          </div>
+        </div>
+      </div>
+      <!-- /.card -->
+    </section>
+    <!-- /.content -->
+  </div>
+</div>
+
+<!-- DataTables  & Plugins -->
+<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="../../plugins/jszip/jszip.min.js"></script>
+<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
+<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="{{ asset('assets/assets/vendor_components/ckeditor/ckeditor.js') }}"></script>
+<script src="https://cdn.ckeditor.com/ckeditor5/31.0.0/classic/ckeditor.js"></script>
+
+<!-- Page specific script -->
+<script src="{{ asset('assets/src/js/pages/data-table.js') }}"></script>
+
+<script>
+     $(document).ready( function () {
+        $('#myTable').DataTable({
+          dom: 'lBfrtip', // if you remove this line you will see the show entries dropdown
+          
+          buttons: [
+              'copy', 'csv', 'excel', 'pdf', 'print'
+          ],
+        });
+    } );
+  </script>
+
+  <script type="text/javascript">
+    var from = '';
+    var to = '';
+
+    $(document).on('change', '#from', async function(e){
+        from = $(e.target).val();
+
+        await getStudent(from,to);
+      });
+
+      $(document).on('change', '#to', async function(e){
+        to = $(e.target).val();
+
+        await getStudent(from,to);
+      });
+
+
+  function getStudent(from,to)
+  {
+    return $.ajax({
+            headers: {'X-CSRF-TOKEN':  $('meta[name="csrf-token"]').attr('content')},
+            url      : "{{ url('coop/voucher/report/dailyreport/getDailyReport') }}",
+            method   : 'GET',
+            data 	 : {from: from, to: to},
+            error:function(err){
+                alert("Error");
+                console.log(err);
+            },
+            success  : function(data){
+              var d = new Date();
+
+              var month = d.getMonth()+1;
+              var day = d.getDate();
+
+              var output = d.getFullYear() + '/' +
+                  (month<10 ? '0' : '') + month + '/' +
+                  (day<10 ? '0' : '') + day;
+
+
+                $('#form-student').html(data);
+            
+                $('#voucher_table').DataTable({
+                  dom: 'lBfrtip', // if you remove this line you will see the show entries dropdown
+                  
+                  buttons: [
+                    {
+                        extend: 'excelHtml5',
+                        messageTop: output,
+                        title: 'Excel' + '-' + output,
+                        text:'Export to excel'
+                        //Columns to export
+                        //exportOptions: {
+                       //     columns: [0, 1, 2, 3,4,5,6]
+                       // }
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        title: 'PDF' + '-' + output,
+                        text: 'Export to PDF'
+                        //Columns to export
+                        //exportOptions: {
+                       //     columns: [0, 1, 2, 3, 4, 5, 6]
+                      //  }
+                    }
+                  ],
+                });
+                //$('#student').selectpicker('refresh');
+
+                "use strict";
+                ClassicEditor
+                .create( document.querySelector( '#commenttxt' ),{ height: '25em' } )
+                .then(newEditor =>{editor = newEditor;})
+                .catch( error => { console.log( error );});
+            }
+        });
+
+  }
+  
+  $(document).ready(function() {
+    $('#printButton').on('click', function(e) {
+      e.preventDefault();
+      printReport();
+    });
+  });
+
+  function printReport() {
+    var from = $('#from').val();
+    var to = $('#to').val();
+
+    return $.ajax({
+      headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+      url: "{{ url('finance/report/dailyreport/getDailyReport?print=true') }}",
+      method: 'GET',
+      data: { from: from, to: to },
+      error: function(err) {
+        alert("Error");
+        console.log(err);
+      },
+      success: function(data) {
+        var newWindow = window.open();
+        newWindow.document.write(data);
+        newWindow.document.close();
+      }
+    });
+  }
+  </script>
+@endsection
---
+++ resources/views/coop/voucher/getDailyReport.blade.php
@@ -0,0 +1,85 @@
+<!-- voucher -->
+<div class="card mb-3" id="stud_info">
+  <div class="card-header">
+  <b>Voucher List</b>
+  </div>
+  <div class="card-body p-0">
+    <table id="myTable" class="table table-striped projects display dataTable">
+      <thead>
+          <tr>
+              <th style="width: 1%">
+                  No.
+              </th>
+              <th style="width: 15%">
+                  Student Name
+              </th>
+              <th style="width: 5%">
+                  Student Ic
+              </th>
+              <th style="width: 5%">
+                  Date Add
+              </th>
+              <th style="width: 5%">
+                  No. Voucher
+              </th>
+              <th style="width: 5%">
+                  Status
+              </th>
+              <th style="width: 5%">
+                  Staff Add
+              </th>
+              <th style="width: 5%">
+                  Redeem Date
+              </th>
+              <th style="width: 5%">
+                  Amount
+              </th>
+          </tr>
+      </thead>
+      <tbody id="table">
+      @foreach ($data['voucher'] as $key => $vcr)
+        <tr>
+          <td>
+          {{ $key+1 }}
+          </td>
+          <td>
+          {{ $vcr->student }}
+          </td>
+          <td>
+          {{ $vcr->student_ic }}
+          </td>
+          <td>
+          {{ $vcr->add_date }}
+          </td>
+          <td>
+          {{ $vcr->no_voucher }}
+          </td>
+          <td>
+          {{ $vcr->status }}
+          </td>
+          <td>
+          {{ $vcr->staff }}
+          </td>
+          <td>
+          {{ $vcr->redeem_date }}
+          </td>
+          <td>
+          {{ $vcr->amount }}
+          </td>
+        </tr>
+      @endforeach
+      </tbody>
+      <tfoot>
+          <tr>
+              <td colspan="8" style="text-align: center">
+                  TOTAL
+              </td>
+              <td>
+                  {{  $data['sum'] }}
+              </td>
+            </tr>
+      </tfoot>
+    </table>
+  </div>
+  <!-- /.card-body -->
+</div>
