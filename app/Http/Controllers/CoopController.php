d233db2881ff1dbb994e04850d5fdd3e4b274886
hafiy <hafiyyaimann1998@gmail.com>
2023-06-26 12:46:07 +0800

    upt coop C

--- app/Http/Controllers/CoopController.php
+++ app/Http/Controllers/CoopController.php
@@ -27,4 +27,63 @@ class CoopController extends Controller
         return view('dashboard');
     }
 
+    public function voucher()
+    {
+
+        return view('coop.voucher.voucher');
+
+    }
+
+    private function getVoucherData($voucher) {
+        return DB::table('tblstudent_voucher')
+                  ->join('tblprocess_status', 'tblstudent_voucher.status', 'tblprocess_status.id')
+                  ->select('tblstudent_voucher.*', 'tblprocess_status.name AS status')
+                  ->where('tblstudent_voucher.no_voucher', $voucher)->first();
+    }
+
+    public function findVoucher(Request $request)
+    {
+
+        $data['voucher'] = $this->getVoucherData($request->search);
+
+        if($data['voucher'] != null)
+        {
+            
+            $data['student'] = DB::table('students')
+                           ->join('tblstudent_status', 'students.status', 'tblstudent_status.id')
+                           ->join('tblprogramme', 'students.program', 'tblprogramme.id')
+                           ->select('students.*', 'tblstudent_status.name AS status', 'tblprogramme.progname AS program')
+                           ->where('ic', $data['voucher']->student_ic)->first();
+
+            return view('coop.voucher.voucherGetStudent', compact('data'));
+
+        }else{
+
+            return ["message" => "Error"];
+
+        }
+
+    }
+
+    public function redeemVoucher(Request $request)
+    {
+
+        DB::table('tblstudent_voucher')->where('id', $request->id)->update([
+            'status' => 2,
+            'redeem_date' => date('Y-m-d')
+        ]);
+
+        $voucher = DB::table('tblstudent_voucher')->where('id', $request->id)->first();
+
+        $data['voucher'] = $this->getVoucherData($voucher->no_voucher);
+
+        $data['student'] = DB::table('students')
+                           ->join('tblstudent_status', 'students.status', 'tblstudent_status.id')
+                           ->join('tblprogramme', 'students.program', 'tblprogramme.id')
+                           ->select('students.*', 'tblstudent_status.name AS status', 'tblprogramme.progname AS program')
+                           ->where('ic', $data['voucher']->student_ic)->first();
+
+        return view('coop.voucher.voucherGetStudent', compact('data'));
+    }
+
 }
