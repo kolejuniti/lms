51c8afefb2ba9fc5bcd803d9fe05351bb7abc3b8
hafiy <hafiyyaimann1998@gmail.com>
2023-06-26 12:46:46 +0800

    add coop folder

---
+++ resources/views/coop/voucher/voucher.blade.php
@@ -0,0 +1,241 @@
+@extends('../layouts.coop')
+
+@section('main')
+<!-- Content Header (Page header) -->
+<div class="content-wrapper" style="min-height: 695.8px;">
+  <div class="container-full">
+  <!-- Content Header (Page header) -->	  
+  <div class="content-header">
+    <div class="d-flex align-items-center">
+      <div class="me-auto">
+        <h4 class="page-title">Voucher</h4>
+        <div class="d-inline-block align-items-center">
+          <nav>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+              <li class="breadcrumb-item" aria-current="page">Dashboard</li>
+              <li class="breadcrumb-item active" aria-current="page">Voucher</li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Student Voucher</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card mb-3">
+                <div class="card-header">
+                  <b>Search Voucher</b>
+                </div>
+                <div class="card-body">
+                  <div class="row">
+                      <div class="col-md-6">
+                          <div class="form-group">
+                          <label class="form-label" for="name">No. Voucher</label>
+                          <input type="text" class="form-control" id="search" placeholder="Search..." name="search">
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row" id="confirm-card" >
+                    <div class="col-md-12 mt-3 d-flex justify-content-end">
+                        <div class="form-group mt-3">
+                          <button type="submit" class="btn btn-primary mb-3" onclick="confirm()">Find</button>
+                        </div>
+                    </div>       
+                  </div>
+                  <div class="card">
+                    <div id="form-voucher">
+              
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+</div>
+
+<script src="{{ asset('assets/assets/vendor_components/ckeditor/ckeditor.js') }}"></script>
+<script src="https://cdn.ckeditor.com/ckeditor5/31.0.0/classic/ckeditor.js"></script>
+<script type="text/javascript">
+
+function confirm()
+{
+
+  var search = $('#search').val();
+
+  return $.ajax({
+            headers: {'X-CSRF-TOKEN':  $('meta[name="csrf-token"]').attr('content')},
+            url      : "{{ url('coop/voucher/findVoucher') }}",
+            method   : 'POST',
+            data 	 : {search: search},
+            error:function(err){
+                alert("Error");
+                console.log(err);
+            },
+            success  : function(data){
+
+                if(data.message)
+                {
+
+                  alert('Voucher does not exist or not registered, please check again!');
+
+                }else{
+
+                  $('#form-voucher').html(data);
+
+                }
+                
+            }
+        });
+
+}
+
+
+function redeem(id)
+{
+
+  return $.ajax({
+            headers: {'X-CSRF-TOKEN':  $('meta[name="csrf-token"]').attr('content')},
+            url      : "{{ url('coop/voucher/redeemVoucher') }}",
+            method   : 'POST',
+            data 	 : {id: id},
+            error:function(err){
+                alert("Error");
+                console.log(err);
+            },
+            success  : function(data){
+
+                if(data.message)
+                {
+
+                  alert('Voucher does not exist or not registered, please check again!');
+
+                }else{
+
+                  $('#form-voucher').html(data);
+
+                }
+                
+            }
+        });
+
+}
+
+
+
+function add(ic)
+{
+  if($('#idpayment').val() != '')
+  {
+
+    var forminput = [];
+    var formData = new FormData();
+
+    forminput = {
+      id: $('#idpayment').val(),
+      type: $('#type').val(),
+      method: $('#method').val(),
+      bank: $('#bank').val(),
+      nodoc: $('#nodoc').val(),
+      amount: $('#amount').val(),
+    };
+
+    formData.append('paymentDetail', JSON.stringify(forminput));
+
+    $.ajax({
+          headers: {'X-CSRF-TOKEN':  $('meta[name="csrf-token"]').attr('content')},
+          url: '{{ url('/finance/payment/storePaymentDtl') }}',
+          type: 'POST',
+          data: formData,
+          cache : false,
+          processData: false,
+          contentType: false,
+          error:function(err){
+              console.log(err);
+          },
+          success:function(res){
+              try{
+                  if(res.message == "Success"){
+                      alert("Success! Payment Details has been added!");
+                      $('#payment_list').html(res.data);
+                      $('#payment_list').DataTable();
+                  }else{
+                      $('.error-field').html('');
+                      if(res.message == "Field Error"){
+                          for (f in res.error) {
+                              $('#'+f+'_error').html(res.error[f]);
+                          }
+                      }
+                      else if(res.message == "Please fill all required field!"){
+                          alert(res.message);
+                      }
+                      else{
+                          alert(res.message);
+                      }
+                      $("html, body").animate({ scrollTop: 0 }, "fast");
+                  }
+              }catch(err){
+                  alert("Ops sorry, there is an error");
+              }
+          }
+      });
+
+  }else{
+
+    alert('Please save payment details first!');
+
+  }
+
+
+}
+
+function deletedtl(dtl,meth,id)
+{
+
+  Swal.fire({
+    title: "Are you sure?",
+    text: "This will be permanent",
+    showCancelButton: true,
+    confirmButtonText: "Yes, delete it!"
+  }).then(function(res){
+    
+    if (res.isConfirmed){
+              $.ajax({
+                  headers: {'X-CSRF-TOKEN':  $('meta[name="csrf-token"]').attr('content')},
+                  url      : "{{ url('finance/payment/deletePayment') }}",
+                  method   : 'POST',
+                  data 	 : {dtl:dtl, meth: meth, id: id},
+                  error:function(err){
+                      alert("Error");
+                      console.log(err);
+                  },
+                  success  : function(data){
+                      alert("success");
+                      $('#payment_list').html(data);
+                      $('#payment_list').DataTable();
+                  }
+              });
+          }
+      });
+
+}
+
+
+</script>
+@endsection
---
+++ resources/views/coop/voucher/voucherGetStudent.blade.php
@@ -0,0 +1,72 @@
+<!-- form start -->
+    <div class="card mb-3" id="stud_info">
+        <div class="card-header">
+        <b>Student Info</b>
+        </div>
+        <div class="card-body">
+            <div class="row mb-5">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <p>Student Name &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp; {{ $data['student']->name }}</p>
+                    </div>
+                    <div class="form-group">
+                        <p>Status &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp; {{ $data['student']->status }}</p>
+                    </div>
+                    <div class="form-group">
+                        <p>Program &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp; {{ $data['student']->program }}</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <p>No. IC / No. Passport &nbsp; &nbsp;: &nbsp;&nbsp; {{ $data['student']->ic }}</p>
+                    </div>
+                    <div class="form-group">
+                        <p>No. Matric &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp; {{ $data['student']->no_matric }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mb-3" id="stud_info">
+        <div class="card-header {{ ($data['voucher']->status == 'BARU') ? 'bg-success' : 'bg-warning' }}">
+        <b>Voucher Details</b>
+        </div>
+        <div class="card-body">
+            <div class="row">       
+                <div class="col-md-12" id="payment-card">
+                    <div class="form-group">
+                        <label class="form-label" for="voucher">No. Voucher</label>
+                        <input type="text" class="form-control" name="voucher" id="voucher" value="{{ $data['voucher']->no_voucher }}" readonly>
+                    </div>
+                </div> 
+                <div class="col-md-12" id="payment-card">
+                    <div class="form-group">
+                        <label class="form-label" for="amount">Amount</label>
+                        <input type="number" class="form-control" name="amount" id="amount" value="{{ $data['voucher']->amount }}" readonly>
+                    </div>
+                </div>
+                <div class="col-md-6" id="payment-card">
+                    <div class="form-group">
+                        <label class="form-label" for="status">Status</label>
+                        <input type="text" class="form-control" name="status" id="status" value="{{ $data['voucher']->status }}" readonly>
+                    </div>
+                </div> 
+                <div class="col-md-6" id="payment-card">
+                    <div class="form-group">
+                        <label class="form-label" for="date">Redeem Date</label>
+                        <input type="date" class="form-control" name="date" id="date" value="{{ $data['voucher']->redeem_date }}" readonly>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer {{ ($data['voucher']->status == 'BARU') ? 'bg-success' : 'bg-warning' }}">
+            <div class="row">
+                <div class="col-md-12 mt-3">
+                    <div class="form-group mt-3">
+                        <button type="submit" class="btn btn-primary pull-right mb-3" onclick="redeem('{{ $data['voucher']->id }}')">Redeem</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
