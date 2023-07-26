fff3c38290b2f1496e5bd948202d20c3d826c374
hafiy <hafiyyaimann1998@gmail.com>
2023-07-26 11:27:22 +0800

    upt finance C

--- resources/views/finance/report/statementGetStudent.blade.php
+++ resources/views/finance/report/statementGetStudent.blade.php
@@ -95,7 +95,7 @@
                                     @endif
                                     </td>
                                     <td>
-                                    @if (array_intersect([1,6,7,8,9,15,16], (array) $req->process_type_id))
+                                    @if (array_intersect([1,6,7,8,9,15,16,17], (array) $req->process_type_id))
                                     {{ number_format($req->amount, 2) }}
                                     @else
                                     0.00
