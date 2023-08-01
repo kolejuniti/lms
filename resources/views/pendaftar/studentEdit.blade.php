bc6bc69e1fce13b3f4e39e21b607a5c45ae15108
hafiy <hafiyyaimann1998@gmail.com>
2023-08-01 12:51:26 +0800

    upt studentEdit

--- resources/views/pendaftar/studentEdit.blade.php
+++ resources/views/pendaftar/studentEdit.blade.php
@@ -297,8 +297,21 @@
 
   function getProgram(ic)
   {
-    alert(ic);
-   
+    return $.ajax({
+            headers: {'X-CSRF-TOKEN':  $('meta[name="csrf-token"]').attr('content')},
+            url      : "{{ url('pendaftar/getProgram') }}",
+            method   : 'POST',
+            data 	 : {ic: ic},
+            error:function(err){
+                alert("Error");
+                console.log(err);
+            },
+            success  : function(data){
+                $('#getModal').html(data);
+                $('#uploadModal').modal('show');
+            }
+        });
+
   }
 
   $('#search').keyup(function(event){
