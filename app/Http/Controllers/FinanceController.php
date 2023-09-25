41e7a24667d5453a7260c8fda01e0fba5fa9e1b4
hafiy <hafiyyaimann1998@gmail.com>
2023-09-25 17:36:10 +0800

    upt finance C

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -385,7 +385,8 @@ class FinanceController extends Controller
 
         $data['method'] = DB::table('tblpayment_method')->get();
 
-        $data['bank'] = DB::table('tblpayment_bank')->orderBy('name', 'asc')->get();
+        $data['bank'] = DB::table('tblpayment_bank')
+                        ->select('tblpayment_bank.*', 'tblpayment_bank.code AS name')->orderBy('name', 'asc')->get();
 
         return  view('finance.payment.paymentGetStudent', compact('data'));
 
