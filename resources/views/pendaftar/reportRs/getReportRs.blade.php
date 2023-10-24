3f8cffce3dff4d824bde482073ce3a597d935d75
hafiy <hafiyyaimann1998@gmail.com>
2023-10-24 15:40:31 +0800

    upt pendaftar report Rs

---
+++ resources/views/pendaftar/reportRs/getReportRs.blade.php
@@ -0,0 +1,217 @@
+<div class="card mb-3" id="stud_info">
+  <div class="card-header">
+  <b>Students Report R1</b>
+  </div>
+  <div class="card-body p-0">
+    <table id="myTable" class="table table-striped projects display dataTable">
+      <thead>
+          <tr>
+              <th style="width: 1%">
+                  No.
+              </th>
+              <th style="width: 15%">
+                  Name
+              </th>
+              <th style="width: 5%">
+                  No. IC
+              </th>
+              <th style="width: 5%">
+                  Gender
+              </th>
+              <th style="width: 5%">
+                  No.Matric
+              </th>
+              <th style="width: 5%">
+                  Phone No.
+              </th>
+              <th style="width: 5%">
+                  Intake Session
+              </th>
+              <th style="width: 5%">
+                  Date Offer
+              </th>
+              <th style="width: 5%">
+                  Program
+              </th>
+              <th style="width: 5%">
+                  Status
+              </th>
+              <th style="width: 5%">
+                  EA
+              </th>
+              <th style="width: 5%">
+                  Amount
+              </th>
+              <th style="width: 5%">
+                  Type
+              </th>
+          </tr>
+      </thead>
+      <tbody id="table">
+      @foreach ($data['studentR1'] as $key => $rgs)
+        <tr>
+          <td>
+          {{ $key+1 }}
+          </td>
+          <td>
+          {{ $rgs->name }}
+          </td>
+          <td>
+          {{ $rgs->ic }}
+          </td>
+          <td>
+          {{ $rgs->sex }}
+          </td>
+          <td>
+          {{ $rgs->no_matric }}
+          </td>
+          <td>
+          {{ $rgs->no_tel }}
+          </td>
+          <td>
+          {{ $rgs->SessionName }}
+          </td>
+          <td>
+          {{ $rgs->date_offer }}
+          </td>
+          <td>
+          {{ $rgs->progcode }}
+          </td>
+          <td>
+          {{ $rgs->status }}
+          </td>
+          <td>
+          {{ $rgs->ea }}
+          </td>
+          <td>
+          {{ $data['resultR1'][$key]->group_alias }}
+          </td>
+          <td>
+          {{ $data['resultR1'][$key]->amount }}
+          </td>
+        </tr>
+      @endforeach
+      </tbody>
+      <tfoot>
+          {{-- <tr>
+              <td colspan="9" style="text-align: center">
+                  TOTAL
+              </td>
+              <td>
+                  {{  number_format($totalPreALL, 2) }}
+              </td>
+            </tr> --}}
+      </tfoot>
+    </table>
+  </div>
+  <!-- /.card-body -->
+</div>
+
+<div class="card mb-3" id="stud_info">
+    <div class="card-header">
+    <b>Students Report R2</b>
+    </div>
+    <div class="card-body p-0">
+      <table id="myTable" class="table table-striped projects display dataTable">
+        <thead>
+            <tr>
+                <th style="width: 1%">
+                    No.
+                </th>
+                <th style="width: 15%">
+                    Name
+                </th>
+                <th style="width: 5%">
+                    No. IC
+                </th>
+                <th style="width: 5%">
+                    Gender
+                </th>
+                <th style="width: 5%">
+                    No.Matric
+                </th>
+                <th style="width: 5%">
+                    Phone No.
+                </th>
+                <th style="width: 5%">
+                    Intake Session
+                </th>
+                <th style="width: 5%">
+                    Date Offer
+                </th>
+                <th style="width: 5%">
+                    Program
+                </th>
+                <th style="width: 5%">
+                    Status
+                </th>
+                <th style="width: 5%">
+                    EA
+                </th>
+                <th style="width: 5%">
+                    Amount
+                </th>
+                <th style="width: 5%">
+                    Type
+                </th>
+            </tr>
+        </thead>
+        <tbody id="table">
+        @foreach ($data['studentR2'] as $key => $rgs)
+          <tr>
+            <td>
+            {{ $key+1 }}
+            </td>
+            <td>
+            {{ $rgs->name }}
+            </td>
+            <td>
+            {{ $rgs->ic }}
+            </td>
+            <td>
+            {{ $rgs->sex }}
+            </td>
+            <td>
+            {{ $rgs->no_matric }}
+            </td>
+            <td>
+            {{ $rgs->no_tel }}
+            </td>
+            <td>
+            {{ $rgs->SessionName }}
+            </td>
+            <td>
+            {{ $rgs->date_offer }}
+            </td>
+            <td>
+            {{ $rgs->progcode }}
+            </td>
+            <td>
+            {{ $rgs->status }}
+            </td>
+            <td>
+            {{ $rgs->ea }}
+            </td>
+            <td>
+            {{ $data['resultR2'][$key]->group_alias }}
+            </td>
+            <td>
+            {{ $data['resultR2'][$key]->amount }}
+            </td>
+          </tr>
+        @endforeach
+        </tbody>
+        <tfoot>
+            {{-- <tr>
+                <td colspan="9" style="text-align: center">
+                    TOTAL
+                </td>
+                <td>
+                    {{  number_format($totalPreALL, 2) }}
+                </td>
+              </tr> --}}
+        </tfoot>
+      </table>
+    </div>
+    <!-- /.card-body -->
+  </div>
