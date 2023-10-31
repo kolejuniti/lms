128c13a192bb113ee667725da970f6e44c405779
hafiy <hafiyyaimann1998@gmail.com>
2023-10-31 16:29:41 +0800

    add export

---
+++ app/Exports/TableExport.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+
+class TableExport implements FromView
+{
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function view(): View
+    {
+        return view('quality.report.allReport.exportIndex', [
+            'data' => $this->data
+        ]);
+    }
+}
