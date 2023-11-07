7fa58c62a9e70edf4aa7b431d4d0a5500e04a624
hafiy <hafiyyaimann1998@gmail.com>
2023-11-07 15:52:16 +0800

    upt surat tawaran

--- resources/views/pendaftar/surat_tawaran/surat_tawaran2.blade.php
+++ resources/views/pendaftar/surat_tawaran/surat_tawaran2.blade.php
@@ -33,7 +33,7 @@ use Carbon\Carbon;
     <style>
         @page {
         size: A4; /* reduced height for A5 size in landscape orientation */
-        margin: 1cm;
+        margin: 2cm;
         }
         * {
             margin: 0;
