7928dd188df83a98a6a67c2b7b01a9571b335093
hafiy <hafiyyaimann1998@gmail.com>
2023-11-23 12:31:51 +0800

    upt arrears report

--- resources/views/finance/report/arrearsReport.blade.php
+++ resources/views/finance/report/arrearsReport.blade.php
@@ -348,33 +348,7 @@
               try{
                   if(res.message == "Success"){
                       alert("Success! Status & Student info has been updated!")
-                      $('#complex_header').html(res.data);
-
-                      $('#complex_header').DataTable({
-                        dom: 'lBfrtip', // if you remove this line you will see the show entries dropdown
-                        paging: false,
-
-                        buttons: [
-                            {
-                              text: 'Excel',
-                              action: function () {
-                                // get the HTML table to export
-                                const table = document.getElementById("complex_header");
-                                
-                                // create a new Workbook object
-                                const wb = XLSX.utils.book_new();
-                                
-                                // add a new worksheet to the Workbook object
-                                const ws = XLSX.utils.table_to_sheet(table);
-                                XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-                                
-                                // trigger the download of the Excel file
-                                XLSX.writeFile(wb, "exported-data.xlsx");
-                              }
-                            }
-                        ],
-
-                      });
+                  
 
                       let db = document.getElementById("complex_header");
                       let dbRows = db.rows;
