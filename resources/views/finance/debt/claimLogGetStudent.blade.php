83f42c39b3e9b1a5b869dabd8503ec86a9a1bcff
hafiy <hafiyyaimann1998@gmail.com>
2023-12-08 17:40:01 +0800

    add debt finance

---
+++ resources/views/finance/debt/claimLogGetStudent.blade.php
@@ -0,0 +1,113 @@
+<!-- form start -->
+<div class="card-body">
+    <div class="row">
+        <div class="col-md-12 mt-3">
+            <div class="form-group mt-3">
+                <label class="form-label">Students</label>
+                <table class="w-100 table table-bordered display margin-top-10 w-p100" id="voucher_table">
+                    <thead id="voucher_list">
+                        <tr>
+                            <th style="width: 1%">
+                                No.
+                            </th>
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                IC / Passport No.
+                            </th>
+                            <th>
+                                No. Matric
+                            </th>
+                            <th>
+                                Last Payment
+                            </th>
+                            <th>
+                                Last Amount
+                            </th>
+                            <th>
+                                Contacted Date
+                            </th>
+                            <th>
+                                Expected Payment Date
+                            </th>
+                            <th>
+                                Current Arrears
+                            </th>
+                            <th>
+                                Penbiayaan Khas Arrears
+                            </th>
+                            <th>
+                                Total Arrears
+                            </th>
+                            <th>
+                                Total Days
+                            </th>
+                            <th>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody id="table">
+                        @foreach ($data['student'] as $key => $std)
+                            <tr>
+                            <td>
+                                {{ $key+1 }}
+                            </td>
+                            <th>
+                                {{ $std->name }}
+                            </td>
+                            <th>
+                                {{ $std->ic }}
+                            </td>
+                            <th>
+                                {{ $std->no_matric }}
+                            </td>
+                            <th>
+                                @foreach($data['payment'][$key] as $pym)
+                                {{ $pym->add_date }}
+                                @endforeach
+                            </td>
+                            <th>
+                                @foreach($data['payment'][$key] as $pym)
+                                {{ $pym->amount }}
+                                @endforeach
+                            </td>
+                            <th>
+                                
+                            </td>
+                            <th>
+                                
+                            </td>
+                            <th>
+                                
+                            </td>
+                            <th>
+                                {{ $data['current_balance'][$key] }}
+                            </td>
+                            <th>
+                                {{ $data['pk_balance'][$key] }}
+                            </td>
+                            <th>
+                                {{ $data['total_balance'][$key] }}
+                            </td>
+                            <th>
+                                <a class="btn btn-success btn-sm" href="#" onclick="claimVoucher('{{ $std->ic }}')">
+                                    <i class="ti-check">
+                                    </i>
+                                    Claim
+                                </a>
+                                <a class="btn btn-warning btn-sm" href="#" onclick="unclaimVoucher('{{ $std->ic }}')">
+                                    <i class="ti-close">
+                                    </i>
+                                    Un-Claim
+                                </a>
+                            </td>
+                            </tr>
+                        @endforeach 
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+   
