d70c96c10b761fe01561bcad607477c21dadae81
hafiy <hafiyyaimann1998@gmail.com>
2024-02-13 12:27:18 +0800

    upt finance layout

--- resources/views/layouts/finance.blade.php
+++ resources/views/layouts/finance.blade.php
@@ -299,6 +299,7 @@
 					<li><a href="{{ route('finance.payment.claim') }}" class="{{ (route('finance.payment.claim') == Request::url()) ? 'active' : ''}}">Claims</a></li>
 					<li><a href="{{ route('finance.payment.tuition') }}" class="{{ (route('finance.payment.tuition') == Request::url()) ? 'active' : ''}}">Daily Payment</a></li>
 					<li><a href="{{ route('finance.payment.other') }}" class="{{ (route('finance.payment.other') == Request::url()) ? 'active' : ''}}">Other Payment</a></li>
+					<li><a href="{{ route('finance.payment.incentive') }}" class="{{ (route('finance.payment.incentive') == Request::url()) ? 'active' : ''}}">Incentive Payment</a></li>
 					<li><a href="{{ route('finance.payment.refund') }}" class="{{ (route('finance.payment.refund') == Request::url()) ? 'active' : ''}}">Refund</a></li>
 					<li><a href="{{ route('finance.payment.KWSPrefund') }}" class="{{ (route('finance.payment.KWSPrefund') == Request::url()) ? 'active' : ''}}">KWSP Refund / Discount</a></li>
 					<li><a href="{{ route('finance.payment.cancel') }}" class="{{ (route('finance.payment.cancel') == Request::url()) ? 'active' : ''}}">Cancel Transaction</a></li>
