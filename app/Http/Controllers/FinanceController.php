afe86f7c1a251492769206259aed1a4c78d6176a
hafiy <hafiyyaimann1998@gmail.com>
2024-02-13 13:30:18 +0800

    upt finance C

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -8933,7 +8933,7 @@ class FinanceController extends Controller
 
                     $data['total'] += $req->amount;
                     
-                }elseif(array_intersect([1,6,7,8,9,10,11,12,13,14,15,16,17,18,19], (array) $req->process_type_id))
+                }elseif(array_intersect([1,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25], (array) $req->process_type_id))
                 {
 
                     $data['total'] -= $req->amount;
