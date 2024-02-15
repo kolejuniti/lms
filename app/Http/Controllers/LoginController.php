b078db027abc0bd22799c8d1740c6ad019c04e6b
hafiy <hafiyyaimann1998@gmail.com>
2024-02-15 17:44:40 +0800

    upt login C

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -17,34 +17,44 @@ class LoginController extends Controller
 
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password], $remember))
         {
-            User::where('email', $request->email)->update(['lastLogin' => Carbon::now()]);
 
             $user = User::where('email', $request->email)->first();
 
-            DB::table('tbluser_log')->insert([
-                'ic' => $user->ic,
-                'remark' => 'LOGIN',
-                'date' => Carbon::now()
-            ]);
-                
-            if($user->usrtype == 'PL')
+            if($user->status != 'NOTACTIVE')
             {
-                return redirect()->route('ketua_program');
-            }
-            elseif($user->usrtype == 'AO')
-            {
-                return redirect()->route('pegawai_takbir');
-            }
-            elseif($user->usrtype == 'DN')
-            {
-                return redirect()->route('dekan');
-            }
-            elseif($user->usrtype == 'LCT')
-            {
-                return redirect()->route('lecturer');
-            }else
-            {
-                return back()->with(["message"=>"Please login again using Administrator login!"]);
+
+                User::where('email', $request->email)->update(['lastLogin' => Carbon::now()]);
+
+                DB::table('tbluser_log')->insert([
+                    'ic' => $user->ic,
+                    'remark' => 'LOGIN',
+                    'date' => Carbon::now()
+                ]);
+                    
+                if($user->usrtype == 'PL')
+                {
+                    return redirect()->route('ketua_program');
+                }
+                elseif($user->usrtype == 'AO')
+                {
+                    return redirect()->route('pegawai_takbir');
+                }
+                elseif($user->usrtype == 'DN')
+                {
+                    return redirect()->route('dekan');
+                }
+                elseif($user->usrtype == 'LCT')
+                {
+                    return redirect()->route('lecturer');
+                }else
+                {
+                    return back()->with(["message"=>"Please login again using Administrator login!"]);
+                }
+
+            }else{
+
+                return back()->with(["message"=>"User not active!"]);
+
             }
 
         }else{
@@ -60,39 +70,49 @@ class LoginController extends Controller
 
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password], $remember))
         {
-            User::where('email', $request->email)->update(['lastLogin' => Carbon::now()]);
-            
             $user = User::where('email', $request->email)->first();
-            
-            if($user->usrtype == 'ADM' && $request->usertypes == 'Admin')
+
+            if($user->status != 'NOTACTIVE')
             {
-               return redirect()->route('admin.dashboard');
+
+                User::where('email', $request->email)->update(['lastLogin' => Carbon::now()]);
+                
+                if($user->usrtype == 'ADM' && $request->usertypes == 'Admin')
+                {
+                return redirect()->route('admin.dashboard');
+                }
+                elseif($user->usrtype == 'RGS' && $request->usertypes == 'Pendaftar') {
+                    return redirect()->route('pendaftar.dashboard');
+                }
+                elseif($user->usrtype == 'AR' && $request->usertypes == 'PendaftarAkademik') {
+                    return redirect()->route('pendaftar_akademik.dashboard');
+                }
+                elseif($user->usrtype == 'FN' && $request->usertypes == 'Kewangan')
+                {
+                    return redirect()->route('finance.dashboard');
+                }
+                elseif($user->usrtype == 'TS' && $request->usertypes == 'Bendahari')
+                {
+                    return redirect()->route('treasurer.dashboard');
+                }
+                elseif($user->usrtype == 'OTR' && $request->usertypes == 'Others')
+                {
+                    return redirect()->route('others.dashboard');
+                }
+                elseif($user->usrtype == 'COOP' && $request->usertypes == 'Cooperation')
+                {
+                    return redirect()->route('coop.dashboard');
+                }
+                else{
+                    return back()->with(["message"=>"Not Authorized to Enter!"]);
+                }
+
+            }else{
+
+                return back()->with(["message"=>"User not active!"]);
+
             }
-            elseif($user->usrtype == 'RGS' && $request->usertypes == 'Pendaftar') {
-                return redirect()->route('pendaftar.dashboard');
-            }
-            elseif($user->usrtype == 'AR' && $request->usertypes == 'PendaftarAkademik') {
-                return redirect()->route('pendaftar_akademik.dashboard');
-            }
-            elseif($user->usrtype == 'FN' && $request->usertypes == 'Kewangan')
-            {
-                return redirect()->route('finance.dashboard');
-            }
-            elseif($user->usrtype == 'TS' && $request->usertypes == 'Bendahari')
-            {
-                return redirect()->route('treasurer.dashboard');
-            }
-            elseif($user->usrtype == 'OTR' && $request->usertypes == 'Others')
-            {
-                return redirect()->route('others.dashboard');
-            }
-            elseif($user->usrtype == 'COOP' && $request->usertypes == 'Cooperation')
-            {
-                return redirect()->route('coop.dashboard');
-            }
-            else{
-                return back()->with(["message"=>"Not Authorized to Enter!"]);
-            }
+
         }else{
             return back()->with(["message"=>"Incorrect Email or Password!"]);
         }
