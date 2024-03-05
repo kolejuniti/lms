b8cc97e838e6f9a2f4073eec1ef99bc3c6924795
hafiy <hafiyyaimann1998@gmail.com>
2024-03-05 10:15:39 +0800

    upt surat tawaran

--- resources/views/pendaftar/surat_tawaran/surat_tawaran.blade.php
+++ resources/views/pendaftar/surat_tawaran/surat_tawaran.blade.php
@@ -94,7 +94,7 @@ use Carbon\Carbon;
         <p>Saudara/Saudari,</p>
         <p class="mt-2">TAWARAN KEMASUKAN KE PROGRAM AKADEMIK KOLEJ UNITI SESI <b>{{ $data['student']->intake }}</b></p>
         <p class="mt-2"><b>TAHNIAH</b> dan <b>SUKACITA</b> di maklumkan, saudara/saudari di tawarkan untuk mengikuti pengajian program <b>{{ $data['student']->progname }}</b> di KOLEJ UNITI.</p>
-        <p class="mt-2">Untuk pengetahuan saudara/saudari, program yang diikuti adalah program akademik yang dikendalikan oleh <b>KOLEJ UNITI di PERSIARAN UNITI VILLAGE, TANJUNG AGAS, 71250 PORT DICKSON, NEGERI SEMBILAN DARUL KHUSUS.</b> Setelah memenuhi semua keperluan, saudara/saudari akan <b>DIANUGERAHKAN DIPLOMA OLEH KOLEJ UNITI ATAU UiTM DAN UPM.</b></p>
+        <p class="mt-2">Untuk pengetahuan saudara/saudari, program yang diikuti adalah program akademik yang dikendalikan oleh <b>KOLEJ UNITI di PERSIARAN UNITI VILLAGE, TANJUNG AGAS, 71250 PORT DICKSON, NEGERI SEMBILAN DARUL KHUSUS.</b> Setelah memenuhi semua keperluan, saudara/saudari akan <b>DIANUGERAHKAN DIPLOMA OLEH KOLEJ UNITI ATAU UiTM DAN UTM.</b></p>
         <p class="mt-2">Tawaran ini adalah sah untuk tujuan di atas sahaja.</p>
         <ol class="mt-2">
             <li>Memenuhi syarat kemasukan ke program yang dipohon dan maklumat-maklumat yang diberikan adalah benar. Sekiranya terdapat pemalsuan maklumat dalam borang permohonan dan dokumen yang berkaitan, pihak Kolej UNITI berhak menarik balik tawaran ini atau saudara/i diberhentikan daripada pengajian pada bila-bila masa.</li>
