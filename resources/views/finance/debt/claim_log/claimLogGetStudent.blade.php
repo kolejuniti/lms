7a908daab9f1736338aa76011e2e16d487f3a649
hafiy <hafiyyaimann1998@gmail.com>
2024-03-07 09:39:41 +0800

    upt claimlog

--- resources/views/finance/debt/claim_log/claimLogGetStudent.blade.php
+++ resources/views/finance/debt/claim_log/claimLogGetStudent.blade.php
@@ -50,7 +50,7 @@
                     <tbody id="table">
                         @foreach ($data['student'] as $key => $std)
 
-                            @if(number_format($data['total_balance'][$key], 2) > 0)
+                            @if(number_format($data['current_balance'][$key], 2) > 0 || number_format($data['pk_balance'][$key], 2) > 0 || number_format($data['total_balance'][$key], 2) > 0)
                             <tr>
                                 <td>
                                     {{ $key+1 }}
