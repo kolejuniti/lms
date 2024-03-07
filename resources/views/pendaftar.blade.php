9ef10863ab399f07d173a88885c02751b7c14808
hafiy <hafiyyaimann1998@gmail.com>
2024-03-07 17:26:38 +0800

    upt pendaftar student list

--- resources/views/pendaftar.blade.php
+++ resources/views/pendaftar.blade.php
@@ -1,4 +1,4 @@
-@extends((Auth::user()->usrtype == "RGS") ? 'layouts.pendaftar' : (Auth::user()->usrtype == "FN" ? 'layouts.finance' : (Auth::user()->usrtype == "AR" ? 'layouts.pendaftar_akademik' : '')))
+@extends((Auth::user()->usrtype == "RGS") ? 'layouts.pendaftar' : (Auth::user()->usrtype == "FN" ? 'layouts.finance' : (Auth::user()->usrtype == "AR" ? 'layouts.pendaftar_akademik' : (Auth::user()->usrtype == "UR" ? 'layouts.ur' : ''))))
 
 @section('main')
 <!-- Content Header (Page header) -->
