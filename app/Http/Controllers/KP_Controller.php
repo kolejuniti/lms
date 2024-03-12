09cc6901de0acc1e35a1574b7392a608cb333723
hafiy <hafiyyaimann1998@gmail.com>
2024-03-12 13:07:29 +0800

    upt KP C

--- app/Http/Controllers/KP_Controller.php
+++ app/Http/Controllers/KP_Controller.php
@@ -330,15 +330,15 @@ class KP_Controller extends Controller
                 ->select('student_subjek.*', 'students.name','students.no_matric','sessions.SessionName AS intake')
                 ->join('students', 'student_subjek.student_ic', 'students.ic')
                 ->join('sessions', 'students.intake', 'sessions.SessionID')
-                ->where('courseid',$request->course)
-                ->where('sessionid',$request->session)->get();
+                ->where('student_subjek.courseid',$request->course)
+                ->where('student_subjek.sessionid',$request->session)->get();
             }else
             {
                 $students = DB::table('student_subjek')
                 ->select('student_subjek.*', 'students.name','students.no_matric','sessions.SessionName AS intake')
                 ->join('students', 'student_subjek.student_ic', 'students.ic')
                 ->join('sessions', 'students.intake', 'sessions.SessionID')
-                ->where('sessionid',$request->session)->get();
+                ->where('student_subjek.sessionid',$request->session)->get();
             }
         }
         
