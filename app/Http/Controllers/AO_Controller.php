fd59d494bda6703248b4528a40893cdf5f617012
hafiy <hafiyyaimann1998@gmail.com>
2024-03-12 13:24:07 +0800

    upt AO C

--- app/Http/Controllers/AO_Controller.php
+++ app/Http/Controllers/AO_Controller.php
@@ -15,25 +15,25 @@ class AO_Controller extends Controller
     public function index()
     {
         //forgot current session
-        Session::forget(['User','CourseID','SessionID']);
+        // Session::forget(['User','CourseID','SessionID']);
 
-        Session::put('User', Auth::user());
+        // Session::put('User', Auth::user());
         
-        $ao = Auth::user();
+        // $ao = Auth::user();
 
-        $data = subject::leftjoin('users as A', 'user_subjek.user_ic', '=', 'A.ic')
-                        ->leftjoin('users as B', 'user_subjek.addby', '=', 'B.ic')
-                        ->leftjoin('subjek', 'user_subjek.course_id','=', 'subjek.sub_id')
-                        ->leftjoin('user_program', 'subjek.prgid', 'user_program.program_id')
-                        ->leftjoin('sessions', 'user_subjek.session_id', 'sessions.SessionID')
-                        ->select('A.name as name','B.name as name2','subjek.course_name','subjek.course_code','sessions.SessionName','user_program.id')
-                        ->where('A.faculty', $ao->faculty)
-                        ->where('user_program.user_ic', $ao->ic)
-                        //->orderBy('sessions.SessionID')
-                        ->get();
+        // $data = subject::leftjoin('users as A', 'user_subjek.user_ic', '=', 'A.ic')
+        //                 ->leftjoin('users as B', 'user_subjek.addby', '=', 'B.ic')
+        //                 ->leftjoin('subjek', 'user_subjek.course_id','=', 'subjek.sub_id')
+        //                 ->leftjoin('user_program', 'subjek.prgid', 'user_program.program_id')
+        //                 ->leftjoin('sessions', 'user_subjek.session_id', 'sessions.SessionID')
+        //                 ->select('A.name as name','B.name as name2','subjek.course_name','subjek.course_code','sessions.SessionName','user_subjek.id')
+        //                 ->where('A.faculty', $ao->faculty)
+        //                 ->where('user_program.user_ic', $ao->ic)
+        //                 //->orderBy('sessions.SessionID')
+        //                 ->get();
 
         //dd($data);
 
-        return view('pegawai_takbir', compact('data'));
+        return view('dashboard');
     }
 }
