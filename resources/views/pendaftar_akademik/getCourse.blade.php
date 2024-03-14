6a10040f462d4084f822925ff57e679f6004d789
hafiy <hafiyyaimann1998@gmail.com>
2024-03-14 14:08:52 +0800

    upt addcourse

--- resources/views/pendaftar_akademik/getCourse.blade.php
+++ resources/views/pendaftar_akademik/getCourse.blade.php
@@ -12,12 +12,6 @@
       <div class="row col-md-12">
         <div>
           <div class="form-group">
-            <label>ID</label>
-            <input type="number" name="id" id="id" class="form-control" value="{{ $data['course']->sub_id }}">
-          </div>
-        </div>
-        <div>
-          <div class="form-group">
             <label>Course Name</label>
             <input type="text" name="name" id="name" class="form-control" value="{{ $data['course']->course_name }}">
           </div>
@@ -36,26 +30,28 @@
         </div>
         <div>
           <div class="form-group">
-              <label class="form-label" for="program2">Program</label>
-              <select class="form-select" id="program2" name="program2">
-              <option value="-" selected disabled>-</option>
-                @foreach ($data['program'] as $prg)
-                <option value="{{ $prg->id }}" {{ ($data['course']->prgid == $prg->id) ? 'selected' : '' }}>{{ $prg->progname}}</option> 
+              <label class="form-label" for="prerequisite">Prerequisite</label>
+              <select class="form-select" id="prerequisite" name="prerequisite">
+                <option value="-" selected disabled>-</option>
+                @foreach ($data['courselist'] as $prg)
+                <option value="{{ $prg->sub_id }}" {{ ($data['course']->prerequisite_id == $prg->sub_id) ? 'selected' : '' }}>{{ $prg->course_code}} - {{ $prg->course_name}}</option> 
                 @endforeach
               </select>
           </div>
         </div>
         <div>
           <div class="form-group">
-              <label class="form-label" for="semester">Semester</label>
-              <select class="form-select" id="semester" name="semester">
-              <option value="-" selected disabled>-</option>
-                <option value="1" {{ ($data['course']->semesterid == '1') ? 'selected' : '' }}>Semester 1</option> 
-                <option value="2" {{ ($data['course']->semesterid == '2') ? 'selected' : '' }}>Semester 2</option> 
-                <option value="3" {{ ($data['course']->semesterid == '3') ? 'selected' : '' }}>Semester 3</option> 
-                <option value="4" {{ ($data['course']->semesterid == '4') ? 'selected' : '' }}>Semester 4</option> 
-                <option value="5" {{ ($data['course']->semesterid == '5') ? 'selected' : '' }}>Semester 5</option> 
-                <option value="6" {{ ($data['course']->semesterid == '6') ? 'selected' : '' }}>Semester 6</option> 
+            <label>Course Level ID</label>
+            <input type="number" name="clid" id="clid" class="form-control" value="{{ $data['course']->course_level_id	 }}">
+          </div>
+        </div>
+        <div>
+          <div class="form-group">
+              <label class="form-label" for="offer">Offer</label>
+              <select class="form-select" id="offer" name="offer">
+                <option value="" selected disabled>-</option>
+                <option value="1" {{ ($data['course']->offer == 1) ? 'selected' : '' }}>Offered</option>
+                <option value="0" {{ ($data['course']->offer == 0) ? 'selected' : '' }}>Not Offered</option>
               </select>
           </div>
         </div>
