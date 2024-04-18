e02fca51e1aec85866ad1c02453ed4bcefacc5be
hafiy <hafiyyaimann1998@gmail.com>
2024-04-18 12:25:04 +0800

    upt pendaftar C

--- app/Http/Controllers/PendaftarController.php
+++ app/Http/Controllers/PendaftarController.php
@@ -958,6 +958,7 @@ class PendaftarController extends Controller
                     'student_ic' => $data['id'],
                     'name' => $request->input('w_name')[$i],
                     'ic' => $request->input('w_ic')[$i],
+                    'email' => $request->input('w_email')[$i],
                     'home_tel' => $request->input('w_notel_home')[$i],
                     'phone_tel' => $request->input('w_notel')[$i],
                     'occupation' => $request->input('occupation')[$i],
