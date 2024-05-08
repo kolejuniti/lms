e2d33913257792ee7004910e4b914714f8c99869
hafiy <hafiyyaimann1998@gmail.com>
2024-05-08 10:17:42 +0800

    upt finance C

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -9285,6 +9285,7 @@ class FinanceController extends Controller
                                       ->join('tblstudentclaim', 'tblpaymentdtl.claim_type_id', 'tblstudentclaim.id')
                                       ->where('tblpayment.student_ic', $std->ic)
                                       ->whereBetween('tblpayment.add_date', [$request->from, $request->to])
+                                      ->where('tblpayment.process_status_id', 2)
                                       ->groupBy('tblpayment.student_ic')
                                       ->orderBy('tblpayment.add_date')
                                       ->select('tblpayment.add_date as payment_date', DB::raw('SUM(tblpaymentdtl.amount) as amount'))
