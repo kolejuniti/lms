85e5168ab81814f4357519081eade74e2bc7bce8
hafiy <hafiyyaimann1998@gmail.com>
2024-05-28 16:13:11 +0800

    upt midterm getgroup

--- resources/views/lecturer/courseassessment/midtermstatus.blade.php
+++ resources/views/lecturer/courseassessment/midtermstatus.blade.php
@@ -169,30 +169,29 @@
     $(document).on('change', '#group', function(e) {
         selected_group = $(e.target).val();
 
-        getGroup(selected_group,selected_midterm);
+        getGroup(selected_group);
     });
 
-    function getGroup(group,midterm)
+    function getGroup(group)
     {
 
       return $.ajax({
             headers: {'X-CSRF-TOKEN':  $('meta[name="csrf-token"]').attr('content')},
-            url      : "{{ url('lecturer/midterm2/getStatus') }}",
+            url      : "{{ url('lecturer/midterm/' . request()->id . '/' . request()->midterm . '/getGroup') }}",
             method   : 'POST',
-            data 	 : {group: group,midterm: midterm },
+            data 	 : {group: group},
             error:function(err){
                 alert("Error");
                 console.log(err);
             },
             success  : function(data){
                 
-                //$('#lecturer-selection-div').removeAttr('hidden');
-                //$('#lecturer').selectpicker('refresh');
-      
-                //$('#chapter').removeAttr('hidden');
-                    $('#status').html(data);
-                    $('#myTable').DataTable();
-                    //$('#group').selectpicker('refresh');
+              if(data.message == 'success')
+              {
+              $('#myTable').html(data.content);
+              $('#myTable').DataTable();
+              }
+
             }
         });
 
