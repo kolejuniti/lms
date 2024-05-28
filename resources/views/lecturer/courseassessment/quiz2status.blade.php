4735cb7f4c287e65ec3ba441f38fa647c1b1acac
hafiy <hafiyyaimann1998@gmail.com>
2024-05-28 16:48:36 +0800

    upt quiz status

--- resources/views/lecturer/courseassessment/quiz2status.blade.php
+++ resources/views/lecturer/courseassessment/quiz2status.blade.php
@@ -169,30 +169,29 @@
     $(document).on('change', '#group', function(e) {
         selected_group = $(e.target).val();
 
-        getGroup(selected_group,selected_quiz);
+        getGroup(selected_group);
     });
 
-    function getGroup(group,quiz)
+    function getGroup(group)
     {
 
       return $.ajax({
             headers: {'X-CSRF-TOKEN':  $('meta[name="csrf-token"]').attr('content')},
-            url      : "{{ url('lecturer/quiz2/getStatus') }}",
+            url      : "{{ url('lecturer/quiz2/' . request()->id . '/' . request()->quiz . '/getGroup') }}",
             method   : 'POST',
-            data 	 : {group: group,quiz: quiz },
+            data 	 : {group: group},
             error:function(err){
                 alert("Error");
                 console.log(err);
             },
             success  : function(data){
                 
-                //$('#lecturer-selection-div').removeAttr('hidden');
-                //$('#lecturer').selectpicker('refresh');
-      
-                //$('#chapter').removeAttr('hidden');
-                    $('#status').html(data);
-                    $('#myTable').DataTable();
-                    //$('#group').selectpicker('refresh');
+              if(data.message == 'success')
+              {
+              $('#myTable').html(data.content);
+              $('#myTable').DataTable();
+              }
+
             }
         });
 
