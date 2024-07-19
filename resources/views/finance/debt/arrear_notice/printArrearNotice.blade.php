e1aa692de73bda72ba1e1288c0434b71a0008433
hafiy <hafiyyaimann1998@gmail.com>
2024-07-19 11:03:14 +0800

    upt print arrear notice

--- resources/views/finance/debt/arrear_notice/printArrearNotice.blade.php
+++ resources/views/finance/debt/arrear_notice/printArrearNotice.blade.php
@@ -32,8 +32,8 @@ use Carbon\Carbon;
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <style>
         @page {
-        size: A4; /* reduced height for A5 size in landscape orientation */
-        margin: 1cm;
+            size: A4;
+            margin: 1cm;
         }
         * {
             margin: 0;
@@ -44,14 +44,24 @@ use Carbon\Carbon;
             vertical-align: baseline;
             background: transparent;
             font-size: 12px;
-            
         }
-        body {
-                background-image: url('{{ asset("assets/images/letter_head/letter_head.jpg") }}');
-                background-size: cover; /* Cover the entire page */
-                background-position: center; /* Center the background image */
-                background-repeat: no-repeat; /* Do not repeat the image */
-            }
+        html, body {
+            height: 100%;
+            width: 100%;
+        }
+        body::before {
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-image: url('{{ asset("assets/images/letter_head/letter_head.jpg") }}');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            z-index: -1;
+        }
         h2,h3,p {
             margin: 0;
             padding: 0;
@@ -65,22 +75,20 @@ use Carbon\Carbon;
         .form-group {
             page-break-inside: avoid;
         }
-
         .custom-table, .custom-table th, .custom-table td {
-            border: 1px solid black; /* Adds black grid lines */
+            border: 1px solid black;
         }
-
         .custom-table {
-            width: 100%; /* Ensures the table stretches to the container width */
-            border-collapse: collapse; /* Removes double borders */
+            width: 100%;
+            border-collapse: collapse;
         }
-
         .text-center {
-            text-align: center; /* Centers text */
+            text-align: center;
         }
-
-        </style>
-    </head>
+        .page-break {
+            page-break-before: always;
+        }
+    </style>
     <body>
         {{-- @php
 
@@ -97,8 +105,6 @@ use Carbon\Carbon;
         <br>
         <p>Ruj. Kami : KUSB/UPTP/TAMATPENGAJIAN/2024/{{ $data['student']->no_matric }}</p>
         <p>Tarikh &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : {{ $data['originalDate'] }}</p>
-        <br>
-        <p>Kepada:-</p>
         <div class="col-12 mb-1 mt-1">  
             <div style="border: 1px solid white; padding: 10px;">
             <b>
@@ -139,7 +145,7 @@ use Carbon\Carbon;
                 </tbody>
             </table>
         </div>
-        <p class="mt-2">4. Jika terdapat sebarang pertanyaan, masalah atau perlukan sebarang rayuan ansuran, tuan/puan boleh menghubungi Puan Emi Marlida Binti Jabar / Puan Nur Azwa Najwa Binti Mohamad Zaidi (Unit Penstrukturan Tunggakan Pelajar) di talian 06-6490350.</p>
+        <p class="mt-2">4. Jika terdapat sebarang pertanyaan, masalah atau perlukan sebarang rayuan ansuran, tuan/puan boleh menghubungi Puan Emi Marlida Binti Jabar (Unit Penstrukturan Tunggakan Pelajar) di talian 06-6490350.</p>
         <p class="mt-2">5. Bersama ini disertakan penyata kewangan dan salinan pakej kewangan untuk rujukan pihak tuan/puan. Pelajar juga boleh membuat semakan tunggakan di <b>Portal Pelajar Kolej Uniti</b> dan membuat bayaran menerusi pindahan bank ke akaun kolej (<b>KOLEJ UNITI SDN BHD : Bank Muamalat - 1402 0000 9187 15</b>). Bukti bayaran perlu di whatsapp ke nombor <b>016-9631663</b>. Sila lampirkan nama, nombor matrik dan nombor kad pengenalan pelajar sebagai rujukan pihak kolej.</p>
         <p class="mt-3">Sila sahkan penerimaan notis dan persetujuan ansuran ini. Kegagalan mengesahkan menerima notis ini akan menyebabkan pihak kami tiada pilihan selain menggunakan saluran yang dibenarkan perundangan.</p>
         <p class="mt-3">Segala kerjasama daripada pihak tuan/puan mengesahkan baki tersebut amatlah dihargai</p>
@@ -154,6 +160,82 @@ use Carbon\Carbon;
         <p>Ketua Unit Kewangan Pelajar</p>
         <br>
         <b>KOLEJ UNITI SDN BHD</b></p><br>
+
+        <!-- Page Break -->
+        <div class="page-break"></div>
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="col-12 mb-3"> 
+            <b>
+            <p>KOLEJ UNITI SDN BHD</p>
+            <p>Persiaran Uniti Village,</p>
+            <p>Tanjung Agas,</p>
+            <p>71250 Pasir Panjang,</p>
+            <p>Port Dickson,</p>
+            <p>Negeri Sembilan</p>
+            <br>
+            <p>Ketua Penstrukturan Tunggakan Pelajar</p>
+            <p>(up: EMI MIRLIDA BINTI JABAR)</p>
+            </b>   
+        </div>
+        <p>Tarikh &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : <b>{{ $data['originalDate'] }}</b></p>
+        <br>
+        <p>Tuan/Puan,</p>
+        <p class="mt-2"><i>Assalamualaikum WRT. WBT.</i></p>
+        <p class="mt-2"><b>MAKLUMBALAS PENGESAHAN BAKI TUNGGAKAN YURAN PENGAJIAN DI KOLEJ UNITI</b></p>
+        <p><b>(Rujukan Tuan/Puan : <span style="background-color: black; color: white; padding: 2px;">KUSB/UPTP/TAMATPENGAJIAN/2024/{{ $data['student']->no_matric }}</span>)</b></p>
+        <hr>
+        <p class="mt-2">Dengan segala hormatnya, saya merujuk kepada perkara di atas.</p>
+        <p class="mt-2">2. Saya sedia maklum akan tunggakan yuran di Kolej Uniti seperti berikut :-</p>
+        <div class="col-12 mb-1 mt-1">  
+            <div style="border: 1px solid white; padding: 10px;">
+                <table>
+                    <tr>
+                        <td>Nama</td>
+                        <td>&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;&nbsp;<b>{{ $data['student']->name }}</b></td>
+                    </tr>
+                    <tr>
+                        <td>No. Kad Pengenalan</td>
+                        <td>&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;&nbsp;<b>{{ $data['student']->ic }}</b></td>
+                    </tr>
+                    <tr>
+                        <td>No. Matriks</td>
+                        <td>&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;&nbsp;<b>{{ $data['student']->no_matric }}</b></td>
+                    </tr>
+                    <tr>
+                        <td>Program</td>
+                        <td>&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;&nbsp;<b>{{ $data['student']->code }} - {{ $data['student']->program }}</b></td>
+                    </tr>
+                    <tr>
+                        <td>Baki Tunggakan</td>
+                        <td>&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;&nbsp;<b>RM{{ $data['balance']->balance }}</b></td>
+                    </tr>
+                    <tr>
+                        <td>Rujukan Kami</td>
+                        <td>&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;&nbsp;<b>KUSB/UPTP/TAMATPENGAJIAN/2024/{{ $data['student']->no_matric }}</b></td>
+                    </tr>
+                </table>
+            </div> 
+        </div>
+
+        <p class="mt-2">3. Dengan ini, sata bersetuju untuk membuat bayaran seperti yang termaktub di dalam perjanjian iaitu sebanyak <b>RM {{ $data['details']['money'] }} sebulan selama {{ $data['details']['period'] }} bulan</b> bermula <b>{{ $data['startDate'] }}</b>.</p>
+        <p class="mt-1 mb-1">Sekian, terima kasih.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        {{-- <p style="text-align: center;"><b>[THIS IS A COMPUTER GENERATED AND DOES NOT REQUIRE SIGNATURE]</b></p> --}}
+        <p>Yang benar,</p>
+        <br>
+        <br>
+        <br>
+        <p><b>_____________</b></p>
+        <br>
+        <p><b>{{ $data['student']->name }}</b><br>
+        <p>No. Kad Pengenalan : {{ $data['student']->ic }}</p>
     </body>
 </html>
 
