ccf159659a18139d1c8de29b7e28e8e035e9f0e3
hafiy <hafiyyaimann1998@gmail.com>
2024-07-25 17:29:25 +0800

    upt ^

--- resources/js/app.js
+++ resources/js/app.js
@@ -40,22 +40,7 @@ window.getMessage = function(ic) {
 app.mount('#app');
 
 // Import React setup
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SenateReport from './components/senateReport/senateReport.jsx';
-
-window.renderSenateReport = function(data) {
-
-    const container = document.createElement('div');
-    container.id = 'senateReport';
-    $('#form-student').html(container);
-
-    ReactDOM.render(
-        <SenateReport data={data} />,
-        document.getElementById('senateReport')
-    );
-
-}
+require('./react-app');
 
 
 
