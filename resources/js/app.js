0ed1adab8abfbe7a617a09ef9007ee852acc40f4
hafiy <hafiyyaimann1998@gmail.com>
2024-07-25 23:00:32 +0800

    upt appjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,38 +24,6 @@ window.renderSenateReport = function(data) {
 
 }
 
-import { createApp } from 'vue';
-const app = createApp({});
-
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-import ExampleComponent from './components/TextBox/index.vue';
-app.component('example-component', ExampleComponent);
-
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-// Define a global method to dispatch a custom event
-window.getMessage = function(ic) {
-    const event = new CustomEvent('message-requested', { detail: { ic } });
-    window.dispatchEvent(event);
-};
-
-app.mount('#app');
 
 
 
