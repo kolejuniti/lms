64e11ae61005710aa0ab72e3d885decdd8cd457b
hafiy <hafiyyaimann1998@gmail.com>
2024-08-07 16:52:24 +0800

    upt ^

--- app/Http/Controllers/LecturerController.php
+++ app/Http/Controllers/LecturerController.php
@@ -3165,7 +3165,7 @@ $content .= '</tr>
                                 ['classid', request()->id],
                                 ['sessionid', Session::get('SessionID')]
                             ])->exists()){
-                                dd($totalassign);
+                                dd($percentassign->mark_percentage);
                                 $overallassign[$ky][$keys] = number_format((float)$sumassign[$ky][$keys] / $totalassign * $percentassign->mark_percentage, 2, '.', '');
 
                                 $assigncollection = collect($overallassign[$ky]);
