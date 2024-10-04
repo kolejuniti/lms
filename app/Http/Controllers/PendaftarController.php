e1701e8c329cde4bd9274f516b32d31c98ac3e7d
hafiy <hafiyyaimann1998@gmail.com>
2024-10-04 15:32:51 +0800

    Update PendaftarController.php

--- app/Http/Controllers/PendaftarController.php
+++ app/Http/Controllers/PendaftarController.php
@@ -3087,7 +3087,8 @@ class PendaftarController extends Controller
           $query->select('students.ic')
                 ->from('students')
                 ->where('students.no_matric', '!=', null)
-                ->where('students.status', '<>', 9);
+                ->where('students.status', '<>', 9)
+                ->where('students.campus_id', 1);
         })
         ->where('sessions.Year', $request->year)
         ->distinct()
@@ -3100,6 +3101,7 @@ class PendaftarController extends Controller
             ->select('tblstudent_log.student_ic', DB::raw('MAX(tblstudent_log.id) as latest_id'))
             ->whereIn('tblstudent_log.student_ic', $ic)
             ->where('tblstudent_log.semester_id', 1)
+            ->where('students.campus_id', 1)
             ->whereYear('tblstudent_log.date', '=', $request->year)
             ->groupBy('tblstudent_log.student_ic');
 
@@ -3121,6 +3123,7 @@ class PendaftarController extends Controller
                ->whereIn('tblstudent_log.student_ic', $ic)
                ->whereYear('tblstudent_log.date', '=', $request->year)
                ->where('tblstudent_log.semester_id', '>', 1)
+               ->where('students.campus_id', 1)
                ->groupBy('tblstudent_log.student_ic');
 
         $baseQuery = function () use ($ic, $request) {
