ef2746ed6078f69f26f74a2b4f2f978a28541cac
hafiy <hafiyyaimann1998@gmail.com>
2024-10-21 10:46:07 +0800

    Update AR_Controller.php

--- app/Http/Controllers/AR_Controller.php
+++ app/Http/Controllers/AR_Controller.php
@@ -4136,8 +4136,6 @@ class AR_Controller extends Controller
             3 => 'Wednesday',
             4 => 'Thursday',
             5 => 'Friday',
-            6 => 'Saturday',
-            7 => 'Sunday',
         ];
 
         foreach($data['room'] as $key => $room)
