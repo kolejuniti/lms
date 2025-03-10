6c0b6652fbe9e9dd182254df36f2c73dd96cf762
hafiy <hafiyyaimann1998@gmail.com>
2025-03-10 10:20:29 +0800

    Update AR_Controller.php

--- app/Http/Controllers/AR_Controller.php
+++ app/Http/Controllers/AR_Controller.php
@@ -148,7 +148,7 @@ class AR_Controller extends Controller
                 'course_name' => $data['name'],
                 'course_code' => $data['code'],
                 'course_credit' => $data['credit'],
-                'prerequisite_id' => $data['prerequisite'] ?? null,
+                'prerequisite_id' => $data['prerequisite'] ?? 881,
                 'course_level_id' => $data['clid'],
                 'offer' => $data['offer']
             ]);
@@ -165,7 +165,7 @@ class AR_Controller extends Controller
                 'course_name' => $data['name'],
                 'course_code' => $data['code'],
                 'course_credit' => $data['credit'],
-                'prerequisite_id' => $data['prerequisite'] ?? null,
+                'prerequisite_id' => $data['prerequisite'] ?? 881,
                 'course_level_id' => $data['clid'],
                 'offer' => $data['offer']
             ]);
