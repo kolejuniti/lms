702975c92e812923c3a6473809857e920a9abf22
hafiy <hafiyyaimann1998@gmail.com>
2025-03-14 00:23:12 +0800

    Update FinanceController.php

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -5050,15 +5050,14 @@ class FinanceController extends Controller
 
         // Subquery for tblpaymentdtl with row numbers
         $paymentDtl = DB::table('tblpaymentdtl')
-        ->join('tblpayment', 'tblpaymentdtl.payment_id', 'tblpayment.id')
-        ->join('tblstudentclaim', 'tblpaymentdtl.claim_type_id', 'tblstudentclaim.id')
-        ->whereBetween('tblpaymentdtl.add_date', ['2024-01-02', '2024-01-02'])
-        ->whereIn('tblstudentclaim.groupid', [4])
-        ->select('tblpaymentdtl.id', 'tblpaymentdtl.payment_id', 'tblpaymentdtl.claim_type_id', 'tblpaymentdtl.amount', 'tblpayment.ref_no', 'tblpaymentdtl.add_date', 'tblstudentclaim.name')
-        ->get();
+            ->join('tblstudentclaim', 'tblpaymentdtl.claim_type_id', '=', 'tblstudentclaim.id')
+            ->join('tblpayment', 'tblpaymentdtl.payment_id', '=', 'tblpayment.id')
+            ->whereBetween('tblpayment.add_date', ['2024-01-02', '2024-01-02'])
+            ->whereIn('tblstudentclaim.groupid', [4])
+            ->select('tblpaymentdtl.id', 'tblpaymentdtl.payment_id', 'tblpaymentdtl.claim_type_id', 'tblpaymentdtl.amount', 'tblpayment.red_no', 'tblpayment.add_date', 'tblstudentclaim.name')
+            ->get();
 
         dd($paymentDtl);
-
       
 
         return view('finance.report.dailyReport');
