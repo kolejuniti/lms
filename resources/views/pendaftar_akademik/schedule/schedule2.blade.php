6c0669b11cba18c50eeb6d8cc5c00a10ca8c4c1e
hafiy <hafiyyaimann1998@gmail.com>
2025-03-14 08:53:26 +0800

    Update schedule2.blade.php

--- resources/views/pendaftar_akademik/schedule/schedule2.blade.php
+++ resources/views/pendaftar_akademik/schedule/schedule2.blade.php
@@ -1017,7 +1017,12 @@ function setupCalendar() {
         eventClick: function (info) {
             const eventElement = info.el;
             if (eventElement.getAttribute('data-clicked') === 'true') {
-                openEditEventModal(info.event, calendar);
+                if('{{ request()->type }}' == 'lcr') {
+                    openEditEventModal(info.event, calendar);
+                }else{
+                    // Show event details in a modal or tooltip
+                    showEventDetails(info.event);
+                }
             } else {
                 eventElement.setAttribute('data-clicked', 'true');
                 setTimeout(() => {
@@ -1199,6 +1204,25 @@ function setupCalendar() {
 
     // Initialize button handlers
     initializeButtonHandlers();
+}
+
+/**
+ * Show event details in a modal or tooltip
+ */
+    function showEventDetails(event) {
+    // Create custom tooltip or use SweetAlert for a nice modal
+    Swal.fire({
+        title: event.title,
+        html: `
+            <div class="event-details">
+                <p><strong>Time:</strong> ${event.start.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} - ${event.end ? event.end.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) : 'N/A'}</p>
+                ${event.extendedProps.description ? `<p><strong>Description:</strong> ${event.extendedProps.description}</p>` : ''}
+            </div>
+        `,
+        icon: 'info',
+        confirmButtonText: 'Close',
+        confirmButtonColor: '#4361ee'
+    });
 }
 
 /**
