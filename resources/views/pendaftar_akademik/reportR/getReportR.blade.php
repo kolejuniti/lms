ca92ee4424bfee44aada40b6f685702584bc6571
hafiy <hafiyyaimann1998@gmail.com>
2025-05-06 11:29:26 +0800

    Update getReportR.blade.php

--- resources/views/pendaftar_akademik/reportR/getReportR.blade.php
+++ resources/views/pendaftar_akademik/reportR/getReportR.blade.php
@@ -8,22 +8,87 @@
       <div class="form-group column-toggles">
         <div class="mb-2">
           <button type="button" id="show-all-columns" class="btn btn-primary btn-sm">Show All Columns</button>
-          <button type="button" id="hide-all-columns" class="btn btn-secondary btn-sm">Hide All Columns</button>
+          <button type="button" id="hide-all-columns" class="btn btn-secondary btn-sm ms-2">Hide All Columns</button>
         </div>
         <div class="row">
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="0" checked> Show No.</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="1" checked> Show Name</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="2" checked> Show No. IC</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="3" checked> Show No. Matric</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="4" checked> Show Phone No.</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="5" checked> Show Intake Session</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="6" checked> Show Date Offer</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="7" checked> Show Program</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="8" checked> Show Qualification</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="9" checked> Show Gender</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="10" checked> Show EA</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="11" checked> Show Amount</label></div>
-          <div class="col-md-3 mb-1"><label><input type="checkbox" class="toggle-column" data-column="12" checked> Show Type</label></div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-0" data-column="0" checked>
+              <label class="form-check-label" for="col-0">Show No.</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-1" data-column="1" checked>
+              <label class="form-check-label" for="col-1">Show Name</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-2" data-column="2" checked>
+              <label class="form-check-label" for="col-2">Show No. IC</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-3" data-column="3" checked>
+              <label class="form-check-label" for="col-3">Show No. Matric</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-4" data-column="4" checked>
+              <label class="form-check-label" for="col-4">Show Phone No.</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-5" data-column="5" checked>
+              <label class="form-check-label" for="col-5">Show Intake Session</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-6" data-column="6" checked>
+              <label class="form-check-label" for="col-6">Show Date Offer</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-7" data-column="7" checked>
+              <label class="form-check-label" for="col-7">Show Program</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-8" data-column="8" checked>
+              <label class="form-check-label" for="col-8">Show Qualification</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-9" data-column="9" checked>
+              <label class="form-check-label" for="col-9">Show Gender</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-10" data-column="10" checked>
+              <label class="form-check-label" for="col-10">Show EA</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-11" data-column="11" checked>
+              <label class="form-check-label" for="col-11">Show Amount</label>
+            </div>
+          </div>
+          <div class="col-md-3 mb-1">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input toggle-column" id="col-12" data-column="12" checked>
+              <label class="form-check-label" for="col-12">Show Type</label>
+            </div>
+          </div>
         </div>
       </div>
     </div>
