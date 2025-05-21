ca39cf2687a94844bdaca892b2841fac8c56f35b
hafiy <hafiyyaimann1998@gmail.com>
2025-05-21 17:33:11 +0800

    Update PendaftarController.php

--- app/Http/Controllers/PendaftarController.php
+++ app/Http/Controllers/PendaftarController.php
@@ -3974,49 +3974,72 @@ class PendaftarController extends Controller
         // Add Total Payment By Weeks data
         $sheet->setCellValue('A1', 'Week');
         $sheet->setCellValue('B1', 'Month');
-        $sheet->setCellValue('C1', 'Total');
-        $sheet->setCellValue('D1', 'Converted');
+        $sheet->setCellValue('C1', 'Total By Weeks');
+        $sheet->setCellValue('D1', 'Total by Cumulative');
+        $sheet->setCellValue('E1', 'Total by Convert');
+        $sheet->setCellValue('F1', 'Balance Student');
     
         $row = 2;
         $total_allW = 0;
-        $total_converted = 0;
+        $total_allC = 0;
+        $total_allC2 = 0;
+        $total_allB = 0;
         foreach ($data['dateRange'] as $key => $week) {
             $sheet->setCellValue('A' . $row, $week['week']);
             $sheet->setCellValue('B' . $row, $week['month']);
             $sheet->setCellValue('C' . $row, $data['totalWeek'][$key]->total_week);
-            $sheet->setCellValue('D' . $row, $data['totalConvert'][$key]);
+            $sheet->setCellValue('D' . $row, $data['countedPerWeek'][$key]);
+            $sheet->setCellValue('E' . $row, $data['totalConvert'][$key]);
+            $sheet->setCellValue('F' . $row, $data['totalWeek'][$key]->total_week - $data['totalConvert'][$key]);
+            
             $total_allW += $data['totalWeek'][$key]->total_week;
-            $total_converted += $data['totalConvert'][$key];
+            $total_allC += $data['countedPerWeek'][$key];
+            $total_allC2 += $data['totalConvert'][$key];
+            $total_allB += $data['totalWeek'][$key]->total_week - $data['totalConvert'][$key];
             $row++;
         }
     
         $sheet->setCellValue('A' . $row, 'TOTAL');
-        $sheet->setCellValue('C' . $row, number_format($total_allW, 2));
-        $sheet->setCellValue('D' . $row, number_format($total_converted, 2));
+        $sheet->setCellValue('B' . $row, '');
+        $sheet->setCellValue('C' . $row, $total_allW);
+        $sheet->setCellValue('D' . $row, $total_allC);
+        $sheet->setCellValue('E' . $row, $total_allC2);
+        $sheet->setCellValue('F' . $row, $total_allB);
     
         // Add Total Payment By Days data
         $row += 2; // Add some space between tables
         $sheet->setCellValue('A' . $row, 'Date');
-        $sheet->setCellValue('B' . $row, 'Total');
-        $sheet->setCellValue('C' . $row, 'Converted');
+        $sheet->setCellValue('B' . $row, 'Total By Days');
+        $sheet->setCellValue('C' . $row, 'Total by Cumulative');
+        $sheet->setCellValue('D' . $row, 'Total by Convert');
+        $sheet->setCellValue('E' . $row, 'Balance Student');
     
         $row++;
         $total_allD = 0;
-        $total_convertedD = 0;
+        $total_allQ = 0;
+        $total_allZ = 0;
+        $total_allB = 0;
         foreach ($data['dateRange'] as $key => $week) {
             foreach ($data['week'][$key] as $key2 => $day) {
                 $sheet->setCellValue('A' . $row, $day);
                 $sheet->setCellValue('B' . $row, $data['totalDay'][$key][$key2]->total_day);
-                $sheet->setCellValue('C' . $row, $data['totalConvert2'][$key][$key2]);
+                $sheet->setCellValue('C' . $row, $data['countedPerDay'][$key][$key2]);
+                $sheet->setCellValue('D' . $row, $data['totalConvert2'][$key][$key2]);
+                $sheet->setCellValue('E' . $row, $data['totalDay'][$key][$key2]->total_day - $data['totalConvert2'][$key][$key2]);
+                
                 $total_allD += $data['totalDay'][$key][$key2]->total_day;
-                $total_convertedD += $data['totalConvert2'][$key][$key2];
+                $total_allQ += $data['countedPerDay'][$key][$key2];
+                $total_allZ += $data['totalConvert2'][$key][$key2];
+                $total_allB += $data['totalDay'][$key][$key2]->total_day - $data['totalConvert2'][$key][$key2];
                 $row++;
             }
         }
     
         $sheet->setCellValue('A' . $row, 'TOTAL');
-        $sheet->setCellValue('B' . $row, number_format($total_allD, 2));
-        $sheet->setCellValue('C' . $row, number_format($total_convertedD, 2));
+        $sheet->setCellValue('B' . $row, $total_allD);
+        $sheet->setCellValue('C' . $row, $total_allQ);
+        $sheet->setCellValue('D' . $row, $total_allZ);
+        $sheet->setCellValue('E' . $row, $total_allB);
     
         $writer = new Xlsx($spreadsheet);
         $fileName = 'report.xlsx';
