369b6f78f304a39f1d7f8d7481502f47cb4545f7
hafiy <hafiyyaimann1998@gmail.com>
2025-05-30 12:38:58 +0800

    Update PendaftarController.php

--- app/Http/Controllers/PendaftarController.php
+++ app/Http/Controllers/PendaftarController.php
@@ -5760,6 +5760,7 @@ class PendaftarController extends Controller
                 ->whereNotIn('students.status', [1,14])
                 ->whereYear('p1.add_date', $year)
                 ->whereBetween('students.date_offer', [$fromDate, $toDate])
+                ->groupBy('p1.student_ic')
                 ->get();
 
             }
