4c2766031dad327155ea727807653822fb6f9da3
hafiy <hafiyyaimann1998@gmail.com>
2025-06-12 11:06:46 +0800

    Update index.blade.php

--- resources/views/alluser/message/student_list/index.blade.php
+++ resources/views/alluser/message/student_list/index.blade.php
@@ -168,7 +168,7 @@
 
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Old Message</h3>
+          <h3 class="card-title">Message History</h3>
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
               <i class="fas fa-minus"></i>
