c78fcb61b187bce13cbfe3df6d67dc44189141a2
hafiy <hafiyyaimann1998@gmail.com>
2025-08-20 17:36:16 +0800

    Update monthlyReportGetStudent.blade.php

--- resources/views/finance/debt/monthly_payment_report/monthlyReportGetStudent.blade.php
+++ resources/views/finance/debt/monthly_payment_report/monthlyReportGetStudent.blade.php
@@ -85,24 +85,9 @@
                     </thead>
                     <tbody id="table">
                         @foreach ($data['student'] as $key => $std)
-                            @php
-                                $showStudent = false;
-                                $mainBalance = $data['total_balance'][$key];
-                                $fineBalance = isset($data['fine_balance'][$key]) ? $data['fine_balance'][$key] : 0;
-                                $otherBalance = isset($data['other_balance'][$key]) ? $data['other_balance'][$key] : 0;
-                                
-                                if (isset($data['includeFineOther']) && $data['includeFineOther']) {
-                                    // Include students with any positive balance (main, fine, or other)
-                                    $showStudent = ($mainBalance > 0 || $fineBalance > 0 || $otherBalance > 0);
-                                } else {
-                                    // Original logic - only show students with positive main balance
-                                    $showStudent = ($mainBalance > 0);
-                                }
-                            @endphp
-                            @if($showStudent)
                             <tr>
                                 <td>
-                                    {{ $key+1 }}
+                                    {{ $key+1 }} {{ $data['total_balance'][$key] }}{{ $data['fine_balance'][$key] }} {{ $data['other_balance'][$key] }}
                                 </td>
                                 <td>
                                     {{ $std->name }}
@@ -182,7 +167,6 @@
                                     {{ number_format($data['total_balance'][$key], 2) }}
                                 </td>
                             </tr>
-                            @endif
                         @endforeach 
                     </tbody>
                 </table>
