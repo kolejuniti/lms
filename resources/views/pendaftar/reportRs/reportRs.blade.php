ea363ae9d04b7de775e495b5a94b7088f9244003
hafiy <hafiyyaimann1998@gmail.com>
2025-08-25 10:29:07 +0800

    Update reportRs.blade.php

--- resources/views/pendaftar/reportRs/reportRs.blade.php
+++ resources/views/pendaftar/reportRs/reportRs.blade.php
@@ -161,6 +161,14 @@
                       'copy', 'csv', 'excel', 'pdf', 'print'
                   ],
                 });
+
+                $('#myTable5').DataTable({
+                  dom: 'lBfrtip', // if you remove this line you will see the show entries dropdown
+                  
+                  buttons: [
+                      'copy', 'csv', 'excel', 'pdf', 'print'
+                  ],
+                });
             }
         });
 
