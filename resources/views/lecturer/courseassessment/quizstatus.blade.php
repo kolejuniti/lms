ab6b1ceb264d43725bc8334a31795b20bd573ad0
hafiy <hafiyyaimann1998@gmail.com>
2025-10-29 17:51:40 +0800

    Update quizstatus.blade.php

--- resources/views/lecturer/courseassessment/quizstatus.blade.php
+++ resources/views/lecturer/courseassessment/quizstatus.blade.php
@@ -183,7 +183,7 @@
                                   @endforeach
                                 @else
                                   <td style="width: 20%">
-                                    -
+                                    -sssssssssss
                                   </td>
                                   <td>
                                     -
@@ -192,7 +192,7 @@
                                   -
                                   </td> 
                                   <td class="project-actions text-center">
-                                    {{-- @if(date('Y-m-d H:i:s') > $qz->date_to) --}}
+                                    @if(date('Y-m-d H:i:s') > $qz->date_to)
                                     @php
                                       // Get active assessment period for current user and session
                                       $currentDate = now()->format('Y-m-d');
@@ -232,7 +232,7 @@
                                         </i>
                                         Manual Mark
                                     </a>
-                                    {{-- @endif --}}
+                                    @endif
                                   </td>
                                 @endif
                               </tr> 
