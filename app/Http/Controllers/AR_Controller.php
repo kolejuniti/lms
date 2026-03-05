480da00e6bf67a4cd5344d626004443ca59b9710
faizulsoknan <faizulsoknan@gmail.com>
2026-03-05 11:38:44 +0800

    leftjoin tblcourse_level getStructureReport

--- app/Http/Controllers/AR_Controller.php
+++ app/Http/Controllers/AR_Controller.php
@@ -1284,10 +1284,10 @@ class AR_Controller extends Controller
         $structure = DB::table('subjek_structure')
             ->join('structure', 'subjek_structure.structure', 'structure.id')
             ->join('subjek', 'subjek_structure.courseID', 'subjek.sub_id')
-            ->leftjoin('tblcourse_level', 'subjek.course_level_id', 'tblcourse_level.id')
+            ->join('tblcourse_level', 'subjek.course_level_id', 'tblcourse_level.id')
             ->where('program_id', $request->program)
             ->where('structure.id', $request->structure)
-            ->groupBy('subjek_structure.courseID', 'subjek_structure.semester_id')
+            ->groupBy('structure.structure_name','subjek_structure.semester_id','subjek.course_code',   'subjek.course_name','subjek.course_credit','tblcourse_level.name','subjek_structure.courseID')
             ->orderBy('subjek_structure.semester_id')
             ->select('structure.structure_name', 'subjek_structure.semester_id', 'subjek.course_code', 'subjek.course_name', 'subjek.course_credit', 'tblcourse_level.name AS classification')
             ->get();
