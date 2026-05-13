7dc0e71ac77bbe9d3a65cc7734c6dfff5b615eae
faizulsoknan <faizulsoknan@gmail.com>
2026-05-13 14:24:05 +0800

    update input number into assignment2status

--- resources/views/lecturer/courseassessment/assignment2status.blade.php
+++ resources/views/lecturer/courseassessment/assignment2status.blade.php
@@ -125,7 +125,7 @@
                                 @endif
                                 <td>
                                   <div class="form-inline col-md-6 d-flex">
-                                      <input  type="number" class="form-control" name="marks[]" max="{{ $qz->total_mark}}" value="{{ $status[$key]->final_mark }}">
+                                      <input  type="text" class="form-control" name="marks[]" max="{{ $qz->total_mark}}" value="{{ $status[$key]->final_mark }}">
                                       <input  type="text" name="ic[]" value="{{ $qz->student_ic }}" hidden>
                                       <span>{{ $status[$key]->final_mark }} / {{ $qz->total_mark}}</span>
                                   </div>
