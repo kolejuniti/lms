82b044223e92b6cae442938e9b2a95815d073705
faizulsoknan <faizulsoknan@gmail.com>
2026-06-08 16:58:05 +0800

    add R status for other EA

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -762,7 +762,7 @@ class FinanceController extends Controller
             $updateData = [
                 'location_id' => 1,
                 'status_id' => ($normalizedIc1 !== null && $normalizedIc1 === $normalizedIc2) ? 19 : 11,
-                'reason' => ($normalizedIc1 !== null && $normalizedIc1 === $normalizedIc2) ? 'R - KUPD' : null
+                'reason' => ($normalizedIc1 !== null && $normalizedIc1 === $normalizedIc2) ? 'R - KUPD' : 'R - KUPD (Other EA)',
             ];
 
             // Perform the update
@@ -1648,7 +1648,7 @@ class FinanceController extends Controller
                         'register_at' => now(),
                         'status_id' => ($normalizedIc1 !== null && $normalizedIc1 === $normalizedIc2) ? 20 : 22,
                         'commission' => ($normalizedIc1 !== null && $normalizedIc1 === $normalizedIc2) ? 500 : 0,
-                        'reason' => ($normalizedIc1 !== null && $normalizedIc1 === $normalizedIc2) ? 'R2 - KUPD' : null
+                        'reason' => ($normalizedIc1 !== null && $normalizedIc1 === $normalizedIc2) ? 'R2 - KUPD' : 'R2 - KUPD (Other EA)'
                     ];
 
                     // Perform the update
