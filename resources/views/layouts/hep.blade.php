aa8a85f7b5c522cfc012d94bb3846160c828eed9
faizulsoknan <faizulsoknan@gmail.com>
2026-07-23 14:25:26 +0800

    add vehicle registration sticker

--- resources/views/layouts/hep.blade.php
+++ resources/views/layouts/hep.blade.php
@@ -287,8 +287,6 @@
 					</a>
 					<ul class="treeview-menu treeview-menu-visible" id="treeview-menu-visible">
 						<li><a href="{{ route('kp.vehicle_sticker') }}" class="{{ (route('kp.vehicle_sticker') == Request::url()) ? 'active' : ''}}">Vehicle Sticker Application</a></li>
-					</ul>
-					<ul class="treeview-menu treeview-menu-visible" id="treeview-menu-visible">
 						<li><a href="{{ route('hep.vehicle_sticker.index') }}" class="{{ (route('hep.vehicle_sticker.index') == Request::url()) ? 'active' : ''}}">Vehicle Sticker Records</a></li>
 					</ul>
 				</li>
